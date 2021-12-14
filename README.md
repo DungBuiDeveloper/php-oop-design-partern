887c38f40eb9ad4e4e519f73328d45c80f906581
DUNG BUI TUAN <dungbuituan@DUNGs-MacBook-Pro.local>
2021-12-14 21:18:38 +0700

    ok

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # OOP DESIGN FOR PHP
+
+## Stragery
+
+```
+use \PhpOop\stragery\ConcreteStrategyA;
+use \PhpOop\stragery\ConcreteStrategyB;
+
+use \PhpOop\stragery\Context;
+
+
+$a = new ConcreteStrategyA();
+$b = new ConcreteStrategyB();
+$context = new Context($a);
+echo "Client: Strategy is set to normal sorting.\n";
+$context->doSomeBusinessLogic();
+
+echo "<br/>";
+
+$context = new Context($b);
+echo "Client: Strategy is set to reverse sorting..\n";
+$context->doSomeBusinessLogic();
+
+```
--- index.php
+++ index.php
@@ -1,20 +1,2 @@
 <?
 require_once __DIR__.'/vendor/autoload.php';
-
-use \PhpOop\stragery\ConcreteStrategyA;
-use \PhpOop\stragery\ConcreteStrategyB;
-
-use \PhpOop\stragery\Context;
-
-
-$a = new ConcreteStrategyA();
-$b = new ConcreteStrategyB();
-$context = new Context($a);
-echo "Client: Strategy is set to normal sorting.\n";
-$context->doSomeBusinessLogic();
-
-echo "<br/>";
-
-$context = new Context($b);
-echo "Client: Strategy is set to reverse sorting..\n";
-$context->doSomeBusinessLogic();
